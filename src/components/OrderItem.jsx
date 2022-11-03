1e4e8279a00ff62186d6773b586e15f17f807a04
Juan <yo217@hotmail.es>
2022-11-03 18:48:13 -0500

    deploy

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,34 @@
+import React, {useContext} from 'react';
+import AppContext from "@context/AppContext"
+import '@styles/OrderItem.scss';
+import closeIcon from "@icons/icon_close.png"
+
+const OrderItem = (props) => {
+	const {removeFromCart} = useContext(AppContext)
+	const {product, indexValue} = props
+
+	const handleRemove = index => {
+		removeFromCart(index);
+	}
+
+	return (
+		<div className="OrderItem">
+			<figure>
+				<img 
+					src={product.images[0]} 
+					alt={product.title} 
+				/>
+			</figure>
+			<p>{product.title}</p>
+			<p>${product.price}</p>
+			<img 
+				className="closeIcon"
+				src={closeIcon} 
+				alt="close" 
+				onClick={() => handleRemove(indexValue)}
+			/>
+		</div>
+	);
+}
+
+export default OrderItem;
